374f11a22c9e0976c7d1b591ec9288fda0944792
PiyushDuggal-source <duggalpiyush0@gmail.com>
2022-01-04 22:57:55 +0530

    removes dotenv pkg

--- client/src/components/DashboardInfo.js
+++ client/src/components/DashboardInfo.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-require("dotenv").config();
 
 import { useNavigate } from "react-router-dom";
 import Error from "./Error";
